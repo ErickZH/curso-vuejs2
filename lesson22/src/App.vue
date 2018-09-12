a425c03fc7ecddb6c9efaa1d01bc77750c139d4f
Erick Zarate Hernandez <zaraterick@outlook.com>
2018-09-11 21:37:11 -0500

    Separación de la aplicación de Task en componentes.

--- lesson22/src/App.vue
+++ lesson22/src/App.vue
@@ -1,60 +1,70 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+	<div id="app" class="container">
+		<h2>Tareas</h2>
+
+		<ul class="list-group tasks">
+			<li is="app-task" v-for="(task, index) in tasks" :task="task" :index="index" @remove="deleteTask"></li>
+		</ul>
+
+		<p>
+			<a @click="deleteCompleted">Eliminar tareas completadas</a>
+		</p>
+
+		<form @submit.prevent="createTask" class="new-task-form">
+			<input v-model="new_task" type="text" class="form-control">
+			<button class="btn btn-primary">Crear tarea</button>
+		</form>
+
+		<footer class="footer">
+			<p>&copy; 2017 Erick</p>
+		</footer>
+	</div>
 </template>
 
 <script>
+import Task from './Task.vue'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Curso de Vue.js 2'
-    }
-  }
+	data () {
+		return {
+			new_task: '',
+			tasks: [
+				{
+					description: 'Aprender Vue.js',
+					pending: true
+				},
+				{
+					description: 'Implementar Laravel echo',
+					pending: true
+				},
+				{
+					description: 'Crear un blog con GHost',
+					pending: false
+				}
+			]
+		}
+	},
+	components: {
+		'app-task': Task
+	},
+	methods: {
+		createTask: function() {
+			this.tasks.push({
+				description: this.new_task,
+				pending: true,
+				editing: false
+			});
+
+			this.new_task = '';
+		},
+		deleteCompleted: function() {
+			this.tasks = this.tasks.filter(function(task) {
+				return task.pending;
+			});
+		},
+		deleteTask: function (index) {
+			this.tasks.splice(index, 1);
+		}
+	}
 }
 </script>
-
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-</style>
