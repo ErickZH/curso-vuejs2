52f5b9e3798b99a06851fa088d18a477f8333696
Erick Zarate <zaraterick@outlook.com>
2018-12-09 16:31:56 -0600

    adding news components

--- lesson22/src/App.vue
+++ lesson22/src/App.vue
@@ -2,18 +2,15 @@
 	<div id="app" class="container">
 		<h2>Tareas</h2>
 
-		<ul class="list-group tasks">
-			<li is="app-task" v-for="(task, index) in tasks" :task="task" :index="index" @remove="deleteTask"></li>
-		</ul>
+		<app-task-list :tasks="tasks"></app-task-list>
+
+		<app-task-list :tasks="tasks2"></app-task-list>
 
 		<p>
 			<a @click="deleteCompleted">Eliminar tareas completadas</a>
 		</p>
 
-		<form @submit.prevent="createTask" class="new-task-form">
-			<input v-model="new_task" type="text" class="form-control">
-			<button class="btn btn-primary">Crear tarea</button>
-		</form>
+		<app-task-form @created="createTask"></app-task-form>
 
 		<footer class="footer">
 			<p>&copy; 2017 Erick</p>
@@ -22,7 +19,8 @@
 </template>
 
 <script>
-import Task from './Task.vue'
+import TaskList from './TaskList'
+import TaskForm from './TaskForm'
 
 export default {
 	data () {
@@ -41,29 +39,31 @@ export default {
 					description: 'Crear un blog con GHost',
 					pending: false
 				}
+			],
+			tasks2: [
+				{
+					description: 'Implementar Laravel echo',
+					pending: true
+				},
+				{
+					description: 'Crear un blog con GHost',
+					pending: false
+				}
 			]
 		}
 	},
 	components: {
-		'app-task': Task
+		'app-task-list': TaskList,
+		'app-task-form': TaskForm
 	},
 	methods: {
-		createTask: function() {
-			this.tasks.push({
-				description: this.new_task,
-				pending: true,
-				editing: false
-			});
-
-			this.new_task = '';
-		},
 		deleteCompleted: function() {
 			this.tasks = this.tasks.filter(function(task) {
 				return task.pending;
 			});
 		},
-		deleteTask: function (index) {
-			this.tasks.splice(index, 1);
+		createTask (task) {
+			this.tasks.push(task);
 		}
 	}
 }
---
+++ lesson22/src/TaskList.vue
@@ -0,0 +1,19 @@
+<template>
+    <ul class="list-group tasks">
+        <li is="app-task" v-for="(task, index) in tasks" :task="task" :index="index" @remove="deleteTask"></li>
+    </ul>
+</template>
+
+<script>
+import Task from './Task.vue'
+
+export default {
+    components: { 'app-task': Task},
+    props: ['tasks'],
+    methods: {
+        deleteTask: function (index) {
+			this.tasks.splice(index, 1);
+		}
+    }
+}
+</script>
